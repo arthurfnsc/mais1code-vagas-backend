1eb1135a57c3ac43f2948822db39611831a55ab6
Arthur Fonseca <arthur.fonseca@ilia.digital>
2021-11-04 21:06:56 -0300

    :tada: commit de abertura

---
+++ src/main/java/br/com/maisumcode/vagasbackend/VagasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.maisumcode.vagasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagasBackendApplication.class, args);
+	}
+
+}
